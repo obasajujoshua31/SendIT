df03fd571f3634e521539dfc9e916dec7117c08b
Joshua <obasajujoshua31@gmail.com>
2018-11-19 01:05:53 +0100

    fixed tracked .env

--- server/test/testorders.test.js
+++
@@ -1,9 +0,0 @@
-const testOrder = {
-  placedBy: 23,
-  to: 'Adekunle Ajasin University',
-  from: 'University of Lagos',
-  weight: 20.87,
-  weightMetric: 'Kg',
-};
-
-export default { testOrder };
