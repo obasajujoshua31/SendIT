d4d9319fc4f539f0ef5142e77fb3d84cc98e9dcc
Joshua <obasajujoshua31@gmail.com>
2018-11-21 02:14:33 +0100

    added stuffs

--- server/createuserdb.js
+++ server/createuserdb.js
@@ -7,7 +7,7 @@ const createUserTable = async callback => {
 
   try {
     const sql =
-      'CREATE TABLE users(user_id serial PRIMARY KEY, email VARCHAR (255) NOT NULL, password VARCHAR (255) NOT NULL,  first_name VARCHAR (255) NOT NULL, last_name VARCHAR (255) NOT NULL, is_admin BOOLEAN NOT NULL, registered_on DATE NOT NULL);';
+      'CREATE TABLE users(user_id serial PRIMARY KEY, email VARCHAR (255) NOT NULL, password VARCHAR (255) NOT NULL,  first_name VARCHAR (255) NOT NULL, last_name VARCHAR (255) NOT NULL, is_admin BOOLEAN NOT NULL, registered_on DATE);';
     const results = await client.query(sql);
     console.log(results.rowCount);
   } catch (e) {
