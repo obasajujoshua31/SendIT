0bcb4b4d733de3bfaabc1f1ab08c9eeb3f078cd1
Joshua <obasajujoshua31@gmail.com>
2018-11-22 17:05:30 +0100

    add

--- server/models/getResponseFromDB.js
+++ server/models/getResponseFromDB.js
@@ -2,7 +2,7 @@ import pool from '../config/config';
 
 const getResponseFromDB = (sql, params = null, callback = null) => {
   return pool
-    .query(sql, param)
+    .query(sql, params)
     .then(results => {
       callback(results.rows);
     })
