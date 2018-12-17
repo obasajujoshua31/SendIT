485b37e0734a9ecb968fa823a850db2caab27dc5
Joshua <obasajujoshua31@gmail.com>
2018-12-17 09:32:24 +0100

    set up eslint on frontend

--- javascripts/signUp.js
+++ javascripts/signUp.js
@@ -1,188 +1,190 @@
-const signUpFormElement = document.getElementById("signup_form");
-const signInFormElement = document.getElementById("signin_form1");
-const signInButton = document.getElementById("signInButton");
-const signUpButton = document.getElementById("signUpButton");
-const forgotSignUplink = document.getElementById("forgot_password");
-const forgotPasswordForm = document.getElementById("forgot_password_form");
-const showSignInFormButton = document.getElementById("show_signin_form_button");
+const signUpFormElement = document.getElementById('signup_form');
+const signInFormElement = document.getElementById('signin_form1');
+const signInButton = document.getElementById('signInButton');
+const signUpButton = document.getElementById('signUpButton');
+const forgotSignUplink = document.getElementById('forgot_password');
+const forgotPasswordForm = document.getElementById('forgot_password_form');
+const showSignInFormButton = document.getElementById('show_signin_form_button');
 const newPasswordCreationForm = document.getElementById(
-  "new_password_creation_form"
+  'new_password_creation_form'
 );
 const forgotPasswordSubmitButton = document.getElementById(
-  "forgot_password_submit_button"
+  'forgot_password_submit_button'
 );
 const newPasswordCreationButton = document.getElementById(
-  "new_password_creation_button"
+  'new_password_creation_button'
 );
 
-document.getElementById("createSignUpForm").addEventListener("click", event => {
+document.getElementById('createSignUpForm').addEventListener('click', event => {
   event.preventDefault();
-  signInFormElement.style.display = "none";
-  signUpFormElement.style.display = "block";
+  signInFormElement.style.display = 'none';
+  signUpFormElement.style.display = 'block';
 });
 
-document.getElementById("removeSignUpForm").addEventListener("click", event => {
+document.getElementById('removeSignUpForm').addEventListener('click', event => {
   event.preventDefault();
-  signInFormElement.style.display = "block";
-  signUpFormElement.style.display = "none";
+  signInFormElement.style.display = 'block';
+  signUpFormElement.style.display = 'none';
 });
 
-forgotSignUplink.addEventListener("click", event => {
+forgotSignUplink.addEventListener('click', event => {
   event.preventDefault();
-  forgotPasswordForm.style.display = "block";
-  signInFormElement.style.display = "none";
+  forgotPasswordForm.style.display = 'block';
+  signInFormElement.style.display = 'none';
 });
-showSignInFormButton.addEventListener("click", event => {
+showSignInFormButton.addEventListener('click', event => {
   event.preventDefault();
-  signInFormElement.style.display = "block";
-  forgotPasswordForm.style.display = "none";
+  signInFormElement.style.display = 'block';
+  forgotPasswordForm.style.display = 'none';
 });
 
-signInButton.addEventListener("click", event => {
+signInButton.addEventListener('click', event => {
   event.preventDefault();
 
-  const email = document.getElementById("user_email").value;
-  const password = document.getElementById("user_password").value;
+  const email = document.getElementById('user_email').value;
+  const password = document.getElementById('user_password').value;
 
-  fetch("https://obasajujoshua31.herokuapp.com/api/v1/auth/login", {
-    method: "POST",
-    mode: "cors",
+  fetch('https://obasajujoshua31.herokuapp.com/api/v1/auth/login', {
+    method: 'POST',
+    mode: 'cors',
     body: JSON.stringify({
       email,
-      password
+      password,
     }),
     headers: {
-      "content-type": "application/json"
-    }
+      'content-type': 'application/json',
+    },
   })
     .then(res => res.json())
     .then(res => {
       if (res.success === false) {
-        document.getElementById("server_response_login").innerHTML =
-          "Invalid Email or Password";
+        document.getElementById('server_response_login').innerHTML =
+          'Invalid Email or Password';
       } else {
-        window.localStorage.setItem("sendItToken", "Bearer " + res.token);
-        window.location = "./dashboard.html";
+        window.localStorage.setItem('sendItToken', `Bearer ${res.token}`);
+        window.localStorage.setItem('sendItUserId', res.userObject[0].user_id);
+        window.location = './dashboard.html';
       }
     })
     .catch(e => console.log(e));
 });
 
-signUpButton.addEventListener("click", event => {
+signUpButton.addEventListener('click', event => {
   event.preventDefault();
-  const firstName = document.getElementById("user_signup_first_name").value;
-  const lastName = document.getElementById("user_signup_last_name").value;
-  const email = document.getElementById("user_signup_email").value;
-  const password = document.getElementById("user_signup_password").value;
+  const firstName = document.getElementById('user_signup_first_name').value;
+  const lastName = document.getElementById('user_signup_last_name').value;
+  const email = document.getElementById('user_signup_email').value;
+  const password = document.getElementById('user_signup_password').value;
 
-  fetch("https://obasajujoshua31.herokuapp.com/api/v1/auth/signup", {
-    method: "POST",
-    mode: "cors",
+  fetch('https://obasajujoshua31.herokuapp.com/api/v1/auth/signup', {
+    method: 'POST',
+    mode: 'cors',
     body: JSON.stringify({
       firstName,
       lastName,
       email,
-      password
+      password,
     }),
     headers: {
-      "content-type": "application/json"
-    }
+      'content-type': 'application/json',
+    },
   })
     .then(res => res.json())
     .then(res => {
       if (res.success === false) {
         if (Array.isArray(res.error)) {
-          let errors = "<ul>";
+          let errors = '<ul>';
           res.error.forEach(error => {
             errors += `<li style ="line-height: 20px; text-align: left;">${
               error.msg
             }</li>`;
           });
-          document.getElementById("server_response_signup").innerHTML = errors;
+          document.getElementById('server_response_signup').innerHTML = errors;
         } else {
-          document.getElementById("server_response_signup").innerHTML =
+          document.getElementById('server_response_signup').innerHTML =
             res.error;
         }
       } else {
-        window.localStorage.setItem("sendItToken", "Bearer " + res.token);
-        window.location = "./dashboard.html";
+        window.localStorage.setItem('sendItToken', `Bearer ${res.token}`);
+        window.localStorage.setItem('sendItUserId', res.userObject[0].user_id);
+        window.location = './dashboard.html';
       }
     })
     .catch(e => console.log(e));
 });
-forgotPasswordSubmitButton.addEventListener("click", event => {
+forgotPasswordSubmitButton.addEventListener('click', event => {
   event.preventDefault();
-  const firstName = document.getElementById("forgot_password_first_name").value;
-  const email = document.getElementById("forgot_password_email").value;
+  const firstName = document.getElementById('forgot_password_first_name').value;
+  const email = document.getElementById('forgot_password_email').value;
 
-  fetch("https://obasajujoshua31.herokuapp.com/api/v1/account/recovery", {
-    method: "POST",
-    mode: "cors",
+  fetch('https://obasajujoshua31.herokuapp.com/api/v1/account/recovery', {
+    method: 'POST',
+    mode: 'cors',
     headers: {
-      "content-type": "application/json"
+      'content-type': 'application/json',
     },
     body: JSON.stringify({
       firstName,
-      email
-    })
+      email,
+    }),
   })
     .then(res => res.json())
     .then(res => {
       if (res.success === false) {
         if (Array.isArray(res.error)) {
-          let errors = "<ul>";
+          let errors = '<ul>';
           res.error.forEach(error => {
             errors += `<li style ="line-height: 20px; text-align: left;">${
               error.msg
             }</li>`;
           });
           document.getElementById(
-            "server_response_forgot_password"
+            'server_response_forgot_password'
           ).innerHTML = errors;
         } else {
-          document.getElementById("server_response_forgot_password").innerHTML =
+          document.getElementById('server_response_forgot_password').innerHTML =
             res.error;
         }
       } else {
-        newPasswordCreationForm.style.display = "block";
-        forgotPasswordForm.style.display = "none";
+        newPasswordCreationForm.style.display = 'block';
+        forgotPasswordForm.style.display = 'none';
         document.getElementById(
-          "server_response_create_new_password"
-        ).innerHTML = "Accout Verified Successfully";
+          'server_response_create_new_password_success'
+        ).innerHTML = 'Accout Verified Successfully';
       }
     })
     .catch(error => console.log(error));
 });
 
-newPasswordCreationButton.addEventListener("click", event => {
+newPasswordCreationButton.addEventListener('click', event => {
   event.preventDefault();
-  const password = document.getElementById("user_new_password").value;
+  const password = document.getElementById('user_new_password').value;
   const passwordConfirmation = document.getElementById(
-    "user_new_confirmation_password"
+    'user_new_confirmation_password'
   ).value;
 
-  fetch("https://obasajujoshua31.herokuapp.com/api/v1/account/recovery", {
-    method: "PUT",
-    mode: "cors",
+  fetch('https://obasajujoshua31.herokuapp.com/api/v1/account/recovery', {
+    method: 'PUT',
+    mode: 'cors',
     headers: {
-      "content-type": "application/json"
+      'content-type': 'application/json',
     },
     body: JSON.stringify({
       password,
-      passwordConfirmation
-    })
+      passwordConfirmation,
+    }),
   })
     .then(res => res.json())
     .then(res => {
       if (res.success === false) {
         document.getElementById(
-          "server_response_create_new_password"
+          'server_response_create_new_password'
         ).innerHTML = res.error;
       } else {
-        newPasswordCreationForm.style.display = "none";
-        signInFormElement.style.display = "block";
-        document.getElementById("server_response_login").innerHTML =
-          "Password Changed Successfully";
+        newPasswordCreationForm.style.display = 'none';
+        signInFormElement.style.display = 'block';
+        document.getElementById('server_response_login_success').innerHTML =
+          'Password Changed Successfully';
       }
     })
     .catch(err => console.log(err));
